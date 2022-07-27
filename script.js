c98bb2a360a2fd23c9f7185fef40e4f68cf9088d
soni50649@gmail.com <soni50649@gmail.com>
2022-07-27 15:25:08 +0100

    put in some js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const pokemon = [
+    {name: 'pikachu',
+        age: 5,
+        color: 'yellow',
 
+    },
+    {name: 'bulbasaur',
+    age: 2,
+    color: 'green',
+
+    }
+]
+
+function getCards(){
+    
+}
